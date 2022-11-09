19d64ae90180ccbdb117ddaf2fcdd414aea36218
dimswtj <dimaswitjaksana12@gmail.com>
2022-11-09 14:31:48 +0800

    origin

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Ticket') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12 ">
+        <div class=" max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 ">
+                    <img class="rounded-lg" src="https://i.pinimg.com/564x/c5/70/14/c57014898f572e851ce04158b8c2923d.jpg" alt="kecak">
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 ">
+                    <img class="rounded-lg h-auto" src="https://i.pinimg.com/564x/c5/70/14/c57014898f572e851ce04158b8c2923d.jpg" alt="kecak">
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 ">
+                    <img class="rounded-lg" src="https://i.pinimg.com/564x/c5/70/14/c57014898f572e851ce04158b8c2923d.jpg" alt="kecak">
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
